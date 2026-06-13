d1ddddb8857fe56ac8d7851d61d14c5803f44779
Muhammad Sufian <itzeditz030@gmail.com>
2026-06-13 21:43:07 +0500

    app.py

--- app.py
+++ app.py
@@ -148,6 +148,6 @@ def about():
 
 # Auto-load models when imported (for production/gunicorn)
 load_models()
-app = Flask(__name__)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
